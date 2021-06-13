49e723bd27ce35adccb7e7ddca03d2c0bf1e3f9f
LeonLawrence <leonlawrence@outlook.com>
2021-06-13 22:34:44 +0100

    very little changes changes

--- src/main/java/PokemonManagementSystem.java
+++ src/main/java/PokemonManagementSystem.java
@@ -12,40 +12,44 @@ public class PokemonManagementSystem {
     private MenuSystem menuSystem;
 
     public void pokemonCreator() {
-        while(true) {
-            pokemons.add(new Pokemon("Pikachu", 100));
-            System.out.println(pokemons.size());
-        }
-//        Pokemon pokemon2 = new Pokemon("Charizard", 100);
-//        Pokemon pokemon3 = new Pokemon("Eevee", 100);
-//        Pokemon pokemon4 = new Pokemon("Snorlax", 100);
-//        Pokemon pokemon5 = new Pokemon("Squirtle", 100);
-//        Pokemon pokemon6 = new Pokemon("Lucario", 100);
-//        Pokemon pokemon7 = new Pokemon("Bulbasaur", 100);
-//        Pokemon pokemon8 = new Pokemon("Gyarados", 100);
-//        Pokemon pokemon9 = new Pokemon("Jigglypuff", 100);
-//        Pokemon pokemon10 = new Pokemon("Dragonite", 100);
-//        Pokemon pokemon11 = new Pokemon("Dito", 100);
-//
-//        pokemons.add(pokemon2);
-//        pokemons.add(pokemon3);
-//        pokemons.add(pokemon4);
-//        pokemons.add(pokemon5);
-//        pokemons.add(pokemon6);
-//        pokemons.add(pokemon7);
-//        pokemons.add(pokemon8);
-//        pokemons.add(pokemon9);
-//        pokemons.add(pokemon10);
+//        while(true) {
+//            pokemons.add(new Pokemon("Pikachu", 100));
+//            System.out.println(pokemons.size());
+//        }
+
+        Pokemon pokemon1 = new Pokemon("Pikachu", 100);
+        Pokemon pokemon2 = new Pokemon("Charizard", 100);
+        Pokemon pokemon3 = new Pokemon("Eevee", 100);
+        Pokemon pokemon4 = new Pokemon("Snorlax", 100);
+        Pokemon pokemon5 = new Pokemon("Squirtle", 100);
+        Pokemon pokemon6 = new Pokemon("Lucario", 100);
+        Pokemon pokemon7 = new Pokemon("Bulbasaur", 100);
+        Pokemon pokemon8 = new Pokemon("Gyarados", 100);
+        Pokemon pokemon9 = new Pokemon("Jigglypuff", 100);
+        Pokemon pokemon10 = new Pokemon("Dragonite", 100);
+        Pokemon pokemon11 = new Pokemon("Dito", 100);
+
+        pokemons.add(pokemon1);
+        pokemons.add(pokemon2);
+        pokemons.add(pokemon3);
+        pokemons.add(pokemon4);
+        pokemons.add(pokemon5);
+        pokemons.add(pokemon6);
+        pokemons.add(pokemon7);
+        pokemons.add(pokemon8);
+        pokemons.add(pokemon9);
+        pokemons.add(pokemon10);
     }
 
     public void findRandomPokemon() {
         pokemonCreator();
-        randomisor.randomPokemonPicker(this);
+        randomisor.randomPokemonPicker(this); //this was in param
         bag1.addPokeball(pokeBall1);
     }
 
     public void displayInventory() {
         bag1.showPokeballInBag();
     }
+
 
 }
--- src/main/java/Randomisor.java
+++ src/main/java/Randomisor.java
@@ -3,12 +3,17 @@ import java.util.ArrayList;
 public class Randomisor {
     private PokemonManagementSystem pokemons;
 
-    public void randomPokemonPicker(PokemonManagementSystem pokemonManagementSystem) {
+    public void randomPokemonPickertest(PokemonManagementSystem pokemonManagementSystem) {
         int index = (int) (Math.random() * pokemonManagementSystem.pokemons.size());
         System.out.println("index: " + index);
         System.out.println("Pokemon size: " + pokemonManagementSystem.pokemons.size());
         System.out.println("A wild pokemon appeared: " + pokemonManagementSystem.pokemons.get(index).pokemonInfo());
 
+    }
+
+    public void randomPokemonPicker() {
+        int index = (int) (Math.random());
+        System.out.println("index: " + index);
     }
 
 }
