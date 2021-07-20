0c7d46883115252e669341a08113ef66408a9ac2
LeonLawrence <leonlawrence@outlook.com>
2021-07-20 17:56:15 +0100

    Added more pokemons to pokemon arraylist

--- src/main/java/PokemonManagementSystem.java
+++ src/main/java/PokemonManagementSystem.java
@@ -8,9 +8,25 @@ public class PokemonManagementSystem {
     PokemonManagementSystem() {
         Pokemon pokemon1 = new Pokemon("Pikachu", 100);
         Pokemon pokemon2 = new Pokemon("Charizard", 100);
+        Pokemon pokemon3 = new Pokemon("Eevee", 100);
+        Pokemon pokemon4 = new Pokemon("Snorlax", 100);
+        Pokemon pokemon5 = new Pokemon("Squirtle", 100);
+        Pokemon pokemon6 = new Pokemon("Lucario", 100);
+        Pokemon pokemon7 = new Pokemon("Bulbasaur", 100);
+        Pokemon pokemon8 = new Pokemon("Gyarados", 100);
+        Pokemon pokemon9 = new Pokemon("Jigglypuff", 100);
+        Pokemon pokemon10 = new Pokemon("Dragonite", 100);
 
         pokemons.add(pokemon1);
         pokemons.add(pokemon2);
+        pokemons.add(pokemon3);
+        pokemons.add(pokemon4);
+        pokemons.add(pokemon5);
+        pokemons.add(pokemon6);
+        pokemons.add(pokemon7);
+        pokemons.add(pokemon8);
+        pokemons.add(pokemon9);
+        pokemons.add(pokemon10);
     }
 
     public void addPokeballToBag(Pokeball pokeball) {
